adf4454ac58a4824c51f7bf547f3f7a9df91734e
lu1zibrahim <luizibrahim94@gmail.com>
2020-12-07 21:16:56 -0300

    Floating Point Comparison

---
+++ 2020-12-07/floating_pont_comparison.py
@@ -0,0 +1,8 @@
+def approx_equals(a, b):
+    return abs(a-b) < 0.001
+
+
+
+#This one, I am really, really sad, because I actually gave up, and it was very easy
+#def approx_equals(a, b):
+#    return abs(a - b) < 1e-3

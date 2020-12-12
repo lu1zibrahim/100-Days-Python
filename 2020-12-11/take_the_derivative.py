c1e32a807b2844deac5334de327917fd5108e871
lu1zibrahim <luizibrahim94@gmail.com>
2020-12-11 23:04:35 -0300

    Take the derivative

---
+++ 2020-12-11/take_the_derivative.py
@@ -0,0 +1,6 @@
+def derive(coefficient, exponent):
+    return f"{(coefficient*exponent)}x^{exponent-1}"
+
+#Another way
+"""def derive(coefficient, exponent): 
+    return("{}x^{}".format(coefficient*exponent, exponent-1))"""

11a67c60ed750e37b9fb033f2a8c0cd6dd9e410d
lu1zibrahim <luizibrahim94@gmail.com>
2020-12-13 10:37:09 -0300

    How do I compare numbers?

---
+++ 2020-12-13/how_do_i_compare_numbers.py
@@ -0,0 +1,10 @@
+def what_is(x):
+    if x == 42:
+        return 'everything'
+    elif x == 1764:
+        return 'everything squared'
+    else:
+        return 'nothing'
+
+
+#Not the best way, but the exercise was to debug the code

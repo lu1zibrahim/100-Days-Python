f581d7de30b9e2efea30e8c1db4b42a4ed797dcb
lu1zibrahim <luizibrahim94@gmail.com>
2020-12-15 08:49:15 -0300

    Grasshopper - Create the rooms

---
+++ 2020-12-15/create_the_rooms.py
@@ -0,0 +1,23 @@
+rooms = {
+    "FirstRoom":{
+        "name": "Room One",
+        "description": "Level One",
+        "completed": False,
+    },
+    "SecondRoom":{
+        "name": "Room Two",
+        "description": "Level Two",
+        "completed": False,
+    },
+    "ThirdRoom":{
+        "name": "Room Three",
+        "description": "Level Three",
+        "completed": False,
+    }
+}
+
+#Better Python formating
+"""rooms = { "Room_1":{"Guest_Name":"Hardik","Room_Number":"69","Hotel":"Royal Inn"},
+          "Room_2":{"Guest_Name":"Hridhik","Room_Number":"369","Hotel":"Sandesh The Prince"},
+          "Room_3":{"Guest_Name":"Pratham","Room_Number":"96","Hotel":"The Quorum"},
+          "Room_4":{"Guest_Name":"Ruturaj","Room_Number":"14","Hotel":"Radisson Blu"}}"""

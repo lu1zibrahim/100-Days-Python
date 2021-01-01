a6a0be139f4e817ff07c6ebb55f0123d9975665d
lu1zibrahim <luizibrahim94@gmail.com>
2020-12-31 23:06:19 -0300

    Filter long words

---
+++ 2020-12-31/easy_time_covert.py
@@ -0,0 +1,10 @@
+def time_convert(num):
+    my_time = ""
+    if num <= 0:
+        return "00:00"
+    else:
+        return f"{num//60:02}:{num%60:02}"
+
+#Another way
+"""def timeConvert(m):
+    return '{:02d}:{:02d}'.format(*divmod(max(int(m), 0), 60))   """
